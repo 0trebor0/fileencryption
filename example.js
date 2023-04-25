2c48b11000b9c0060af7b61e6893d9cd7c835734
0trebor0 <git config --global user.email>
2023-04-25 15:01:28 +0100

    commit

---
+++ example.js
@@ -0,0 +1,12 @@
+const encryption = require('./index.js');
+process.env.PRIVATEKEY = '0trebor0';
+
+encryption.encrypt({input:'./LICENSE',output:'./LICENSES2',key:'test'},(e)=>{
+    console.log( e );
+
+    encryption.decrypt({input:'./LICENSES2',output:'./LICENSES3',key:'test', iv:e.iv},( e )=>{
+        console.log( e );
+    });
+
+
+});
